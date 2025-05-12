ebee8906244837ec972fbc7c12b802f1879b66ad
NakulKataria <nakulkofficial@gmail.com>
2025-05-12 08:53:28 +0000

    .

--- 595. Big Countries/solution.sql
+++ 595. Big Countries/solution.sql
@@ -21,4 +21,4 @@ insert into World (name, continent, area, population, gdp) values ('Andorra', 'E
 insert into World (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000')
 
 # Write your MySQL query statement below
-SELECT name, population, area FROM World WHERE area > 3000000 OR population>25000000;
+SELECT name, population, area FROM World WHERE area >= 3000000 OR population>=25000000;
